b511999dbe04c3aaabd48df6179e21836d1d1667
hugosenari <hugosenari@gmail.com>
2022-03-12 04:15:25 -0300

    chore: re index nimpdf

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''library for generating PDF files'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimpdf-master".url = "path:./master";
+  inputs."nimpdf-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimpdf-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
